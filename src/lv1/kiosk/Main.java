44c11b516fe8308e15183d0b36d1266f454320dd
Chaemin07 <gkc05075@gmail.com>
2025-03-10 13:38:56 +0900

    feat: 입력 예외 처리 추가 및 메서드명 변경
    - 정수 입력 예외처리를 위한 판별 메서드 추가
    - 기존 readMenuFromFile 메서드명을 getMenuListFromFile로 변경

--- src/lv1/kiosk/Main.java
+++ src/lv1/kiosk/Main.java
@@ -1,12 +1,10 @@
 package lv1.kiosk;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 // 메뉴 enum import
 import common.Menu;
@@ -14,11 +12,10 @@ import common.Menu;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        boolean mainFlag = false;
+        boolean mainFlag = true;
         String menu = "";
         int selectMenuNum = 0;
         printAsciiArt();
-        printMenu();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 //        URL resource = FileTest.class.getClassLoader().getResource("test.txt");
@@ -41,14 +38,7 @@ public class Main {
 
         // BufferedReader로 파일 입출력
         // 클래스 패스 내부의 리소스만 읽을 경우
-        InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("test.txt");
-        System.out.println("메뉴 출력");
-        // TODO string -> menuItem클래스로 변경 필요
-        List<String> menuList = printMenu(inputStream);
-        for (int i = 0; i < menuList.size(); i++) {
-            System.out.println((i+1)+"번째 메뉴는 "+menuList.get(i));
-        }
-        System.out.println("메뉴를 선택해주세요");
+
 //        String s = br.readLine();
         // 1번부터 입력 -1처리
 //        int idx = Integer.parseInt(s)-1;
@@ -56,22 +46,42 @@ public class Main {
         while (mainFlag) {
             // 메뉴 보기
             printMenu();
-            selectMenuNum = Integer.parseInt(br.readLine());
+            // 설명 출력
+            System.out.println("원하는 메뉴 번호를 입력하세요 (1~3, 종료는 4):");
+            System.out.print("입력 >> "); // 입력 유도 메시지
 
+            //  입력 정수인지 체크 - 예외처리
+            if (isInteger(menu = br.readLine())) {
+                selectMenuNum = Integer.parseInt(menu);
+            }else{
+                continue;
+            }
             try {
                 // 입력받아
                 Menu selectedMenu = Menu.valueOfCode(selectMenuNum);
                 switch (selectedMenu){
                     case VIEW_MENU:     // 음식 메뉴 보기
+                        InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("test.txt");
+                        System.out.println("메뉴 출력");
+                        // TODO string -> menuItem클래스로 변경 필요
+                        List<String> menuList = getMenuListFromFile(inputStream);
+                        // menuList
+                        for (int i = 0; i < menuList.size(); i++) {
+                            System.out.println((i+1)+"번째 메뉴는 "+menuList.get(i));
+                        }
+                        System.out.println("테스트 메뉴를 선택해주세요");
                         break;
 
                     case CART:          // 장바구니
+                        System.out.println("테스트 장바구니 입니다.");
                         break;
 
                     case CHECKOUT:      // 결제하기
+                        System.out.println("테스트 결제 화면입니다.");
                         break;
 
                     case EXIT:          // 종료
+                        mainFlag = false;
                         break;
 
                         // try-catch로 switch문 이외의 입력을 잡을텐데
@@ -103,8 +113,6 @@ public class Main {
                 "$$ |$$  \\  _$$ |_ $$ \\__$$ |/  \\__$$ |$$ |$$  \\ \n" +
                 "$$ | $$  |/ $$   |$$    $$/ $$    $$/ $$ | $$  |\n" +
                 "$$/   $$/ $$$$$$/  $$$$$$/   $$$$$$/  $$/   $$/ \n" +
-                "                                                \n" +
-                "                                                \n" +
                 "                                                \n"
         };
         for (String data : acsiiArt) {
@@ -120,7 +128,8 @@ public class Main {
         System.out.println("3\uFE0F⃣. 결제하기");
         System.out.println("4\uFE0F⃣. 종료: \"exit\", \"q\",\"Q\" 입력하기");
     }
-    static List<String> printMenu(InputStream inputStream) throws Exception {
+    // 
+    static List<String> getMenuListFromFile(InputStream inputStream) throws Exception {
         // Index 0 out of bounds for length 0 인덱스 오류 처리해야함
         List<String> menuList = new ArrayList<>();
         if (inputStream != null) {
@@ -135,6 +144,21 @@ public class Main {
             System.out.println("파일 없음");
         }
         return menuList;
+    }
+
+    static boolean isInteger(String selectMenuNum) {
+        try {
+            // 정수 + 메뉴번호 ( 1 ~ 4 )
+            if((Integer.parseInt(selectMenuNum)<=4) && Integer.parseInt(selectMenuNum)>=1){
+                return true;
+            }else{
+                System.out.println("입력이 올바르지 않습니다!");
+                return false;
+            }
+        } catch (Exception e) {
+            System.out.println("입력이 올바르지 않습니다!");
+            return false;
+        }
     }
 
 }
